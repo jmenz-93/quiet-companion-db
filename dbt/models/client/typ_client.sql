1971ddba51c36f8de35de168ba3561e0741ba723
Jonathan Menzel <jonathanmenzel93@gmail.com>
2026-06-14 07:50:30 -0500

    adding typ client

---
+++ dbt/models/client/typ_client.sql
@@ -0,0 +1,33 @@
+{{ config(
+    materialized='incremental',
+    unique_key=['account_number', 'raw_created_timestamp']
+) }}
+
+SELECT
+    CAST(raw_client.effective_date AS DATE) AS effective_date,
+    raw_client.account_number,
+    raw_client.ssn,
+    raw_client.first_name,
+    raw_client.last_name,
+    CAST(raw_client.date_of_birth AS DATE) AS date_of_birth,
+    raw_client.marital_status,
+    raw_client.number_of_dependents,
+    raw_client.email_address,
+    raw_client.phone_number,
+    raw_client.citizenship_status,
+    raw_client.employment_status,
+    raw_client.occupation,
+    raw_client.employer_name,
+    raw_client.annual_income_bracket,
+    raw_client.estimated_net_worth_bracket,
+    raw_client.education_level,
+    raw_client.politically_exposed_person,
+    raw_client.finra_association,
+    raw_client.aml_flag,
+    raw_client.preferred_contact_method,
+    raw_client.raw_created_timestamp,
+    CURRENT_TIMESTAMP AS typ_created_timestamp
+FROM {{ source('quiet_companion', 'raw_client') }} AS raw_client
+{% if is_incremental() %}
+    WHERE raw_client.raw_created_timestamp >= (SELECT MAX(c.raw_created_timestamp) FROM {{ this }} AS c)
+{% endif %}
