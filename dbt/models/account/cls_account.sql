da83348608bf235a31c452bd6ad1236c7ba0d4c5
Jonathan Menzel <jonathanmenzel93@gmail.com>
2026-06-27 08:13:33 -0500

    changes to qualify

--- dbt/models/account/cls_account.sql
+++ dbt/models/account/cls_account.sql
@@ -5,43 +5,76 @@
     on_schema_change='sync_all_columns'
 )}}
 
+WITH ranked AS (
+    SELECT
+        t.effective_date,
+        t.account_number,
+        t.ssn,
+        p.product_name,
+        p.product_category,
+        p.tax_status,
+        p.contribution_limit_2026,
+        p.income_limit_2026,
+        p.typical_time_horizon,
+        t.account_status,
+        t.date_opened,
+        t.date_closed,
+        t.last_review_date,
+        t.custodian,
+        t.advisor_code,
+        t.investment_objective,
+        t.risk_profile,
+        t.time_horizon,
+        t.rebalance_frequency,
+        t.annual_contribution,
+        t.management_fee,
+        t.margin_enabled,
+        t.options_approved,
+        t.beneficiary_designated,
+        t.esg_preference,
+        t.raw_created_timestamp,
+        t.typ_created_timestamp,
+        ROW_NUMBER() OVER (
+            PARTITION BY t.account_number, t.effective_date
+            ORDER BY t.raw_created_timestamp DESC
+        ) AS row_num
+    FROM {{ref('typ_account')}} AS t
+    LEFT JOIN {{ref("cls_products")}} AS p
+        ON t.product_id = p.product_id
+    WHERE
+        p.product_id IS NOT NULL
+        {% if is_incremental() %}
+            AND t.effective_date >= (SELECT max(t2.effective_date) FROM {{ this }} AS t2)
+        {% endif %}
+)
+
 SELECT
-    t.effective_date,
-    t.account_number,
-    t.ssn,
-    p.product_name,
-    p.product_category,
-    p.tax_status,
-    p.contribution_limit_2026,
-    p.income_limit_2026,
-    p.typical_time_horizon,
-    t.account_status,
-    t.date_opened,
-    t.date_closed,
-    t.last_review_date,
-    t.custodian,
-    t.advisor_code,
-    t.investment_objective,
-    t.risk_profile,
-    t.time_horizon,
-    t.rebalance_frequency,
-    t.annual_contribution,
-    t.management_fee,
-    t.margin_enabled,
-    t.options_approved,
-    t.beneficiary_designated,
-    t.esg_preference,
-    t.raw_created_timestamp,
-    t.typ_created_timestamp
-FROM {{ref('typ_account')}} AS t
-LEFT JOIN {{ref("cls_products")}} AS p
-    ON t.product_id = p.product_id
-WHERE
-    p.product_id IS NOT NULL
-    {% if is_incremental() %}
-        AND t.effective_date >= (SELECT max(t2.effective_date) FROM {{ this }} AS t2)
-    {% endif %}
-QUALIFY ROW_NUMBER() OVER (
-    PARTITION BY t.account_number, t.effective_date
-    ORDER BY t.raw_created_timestamp DESC
-) = 1
+    effective_date,
+    account_number,
+    ssn,
+    product_name,
+    product_category,
+    tax_status,
+    contribution_limit_2026,
+    income_limit_2026,
+    typical_time_horizon,
+    account_status,
+    date_opened,
+    date_closed,
+    last_review_date,
+    custodian,
+    advisor_code,
+    investment_objective,
+    risk_profile,
+    time_horizon,
+    rebalance_frequency,
+    annual_contribution,
+    management_fee,
+    margin_enabled,
+    options_approved,
+    beneficiary_designated,
+    esg_preference,
+    raw_created_timestamp,
+    typ_created_timestamp
+FROM ranked
+WHERE row_num = 1
--- dbt/models/client/cls_client_address.sql
+++ dbt/models/client/cls_client_address.sql
@@ -5,23 +5,43 @@
     on_schema_change='sync_all_columns'
 )}}
 
+WITH ranked AS (
+    SELECT
+        t.effective_date,
+        t.ssn,
+        t.address_line_1,
+        t.address_line_2,
+        t.city,
+        t.state,
+        t.zip_code,
+        t.county,
+        t.country,
+        t.address_type,
+        t.residency_status,
+        t.years_at_current_address,
+        t.raw_created_timestamp,
+        t.typ_created_timestamp,
+        ROW_NUMBER() OVER (PARTITION BY t.ssn, t.effective_date ORDER BY t.raw_created_timestamp DESC) AS row_num
+    FROM {{ref('typ_client_address')}} AS t
+    {% if is_incremental() %}
+        WHERE t.effective_date >= (SELECT max(t2.effective_date) FROM {{ this }} AS t2)
+    {% endif %}
+)
+
 SELECT
-    t.effective_date,
-    t.ssn,
-    t.address_line_1,
-    t.address_line_2,
-    t.city,
-    t.state,
-    t.zip_code,
-    t.county,
-    t.country,
-    t.address_type,
-    t.residency_status,
-    t.years_at_current_address,
-    t.raw_created_timestamp,
-    t.typ_created_timestamp
-FROM {{ref('typ_client_address')}} AS t
-{% if is_incremental() %}
-    WHERE t.effective_date >= (SELECT max(t2.effective_date) FROM {{ this }} AS t2)
-{% endif %}
-QUALIFY ROW_NUMBER()
+    effective_date,
+    ssn,
+    address_line_1,
+    address_line_2,
+    city,
+    state,
+    zip_code,
+    county,
+    country,
+    address_type,
+    residency_status,
+    years_at_current_address,
+    raw_created_timestamp,
+    typ_created_timestamp
+FROM ranked
+WHERE row_num = 1
