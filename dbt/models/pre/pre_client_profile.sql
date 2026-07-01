106d42c0f81ba59163e2b095ad7149e9143f12a8
Jonathan Menzel <jonathanmenzel93@gmail.com>
2026-06-30 20:07:48 -0500

    update pre

--- dbt/models/pre/pre_client_profile.sql
+++ dbt/models/pre/pre_client_profile.sql
@@ -1,28 +1,36 @@
-{{config(materialized='view')}}
-
+WITH latest_client AS (
+    SELECT DISTINCT ON (ssn) *
+    FROM {{ ref('cls_client') }}
+    ORDER BY ssn, effective_date DESC
+),
+latest_address AS (
+    SELECT DISTINCT ON (ssn) *
+    FROM {{ ref('cls_client_address') }}
+    ORDER BY ssn, effective_date DESC
+)
 
 SELECT
-    c.ssn AS client_ssn,
-    c.first_name AS client_first_name,
-    c.last_name AS client_last_name,
-    c.date_of_birth AS client_date_of_birth,
-    c.marital_status AS client_marital_status,
-    c.annual_income_bracket AS client_annual_income_bracket,
-    c.estimated_net_worth_bracket AS client_estimated_net_worth_bracket,
-    c.email_address AS client_email_address,
-    c.occupation AS client_occupation,
-    cca.city AS client_city,
-    cca.state AS client_state,
-    cca.zip_code AS client_zip_code,
-    a.account_number,
-    a.account_status,
-    a.risk_profile,
-    a.investment_objective,
-    a.product_name,
-    a.product_category,
+    c.ssn,
+    c.first_name,
+    c.last_name,
+    c.date_of_birth,
+    c.marital_status,
+    c.annual_income_bracket,
+    c.estimated_net_worth_bracket,
+    c.occupation,
+    a.city,
+    a.state,
+    acc.account_number,
+    acc.account_status,
+    acc.risk_profile,
+    acc.investment_objective,
+    acc.product_id,
+    p.product_name,
+    p.category,
     adv.advisor_name,
-    adv.specialization AS advisor_specialization
-FROM {{ref('cls_client')}} AS c
-INNER JOIN {{ref('cls_client_address')}} AS cca ON c.ssn = cca.ssn
-INNER JOIN {{ref('cls_account')}} AS a ON c.ssn = a.ssn
-INNER JOIN {{ref('cls_advisor')}} AS adv ON a.advisor_code = adv.advisor_code
+    adv.specialization
+FROM latest_client c
+LEFT JOIN latest_address a ON c.ssn = a.ssn
+JOIN {{ ref('cls_accounts') }} acc ON c.ssn = acc.ssn
+JOIN {{ ref('cls_products') }} p ON acc.product_id = p.product_id
+LEFT JOIN {{ ref('cls_advisor') }} adv ON acc.advisor_code = adv.advisor_code
